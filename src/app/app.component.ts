cd79847204248dc0cb04ef1fd16e5112581ff0be
Ayush Agarwal (ayusha) <ayusha@cisco.com>
2018-01-23 12:21:59 +0530

    Module 4 Done.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
+//Decorator - begins with @, using component decorator
+/*
+selector: needs to have a selector to be given a name
+to be used as a directive in the HTML
+template: the view/HTML layout that appears in the directive
+of index.html
+*/
 @Component({
   selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <div><h1>{{pageTitle}}</h1>
+  <div>My first component</div>
+  </div>`
 })
+
+// Class - with property pageTitle of type string and default value
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  pageTitle: string = 'ACME Product Management';
 }
